32e7ca839b7e90f0df290b59940e7f65fe0bbee7
Bobby Liberti <BobbyLiberti@Mes-iMac.local>
2019-08-20 16:20:56 -0400

    finished project and stretch, working on apple nav

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -19,10 +19,10 @@ const Movie = (props) => {
 
   },[id]);
   
-  const saveMovie = () => {
-    const addToSavedList = props.addToSavedList;
-    addToSavedList(movie)
-  }
+  // const saveMovie = () => {
+  //   const addToSavedList = props.addToSavedList;
+  //   addToSavedList(movie)
+  // }
 
   if (!movie) {
     return <div>Loading movie information...</div>;
@@ -47,8 +47,7 @@ const Movie = (props) => {
           </div>
         ))}
       </div>
-      {/* <div onClick={() => props.addToSavedList(movie)} className="save-button">Save</div>
-      {console.log(props.addToSavedList(movie))} */}
+      <div onClick={() => props.addToSavedList(movie)} className="save-button">Save</div>
     </div>
   );
 }
